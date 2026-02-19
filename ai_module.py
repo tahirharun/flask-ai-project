1c6ae432467fa4cf5819e6a2cb77de1a9c1ac935
tahir <haron@gmail.com>
2026-02-18 20:06:18 -0800

    comments removed

--- ai_module.py
+++ ai_module.py
@@ -3,7 +3,6 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 model = GPT2LMHeadModel.from_pretrained("gpt2")
 
-# Conversation memory
 chat_history = []
 
 MAX_MEMORY = 6
